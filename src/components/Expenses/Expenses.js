adc3f03b22aec185e7a864715135405e2506e628
jlvsolmeron <jlvsolmeron@gmail.com>
2021-04-20 21:00:12 +0800

    expenses

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+import ExpenseFilter from './ExpensesFilter';
+import Card from '../UI/Card';
+import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
+import './Expenses.css';
+
+function Expenses(props) {
+    const [filteredYear, setFilteredYear] = useState('2020');
+
+    const filterChangeHandler = selectedYear => {
+        console.log(selectedYear, 'In expenses');
+
+        setFilteredYear(selectedYear);
+    };
+
+    const filteredExpenses = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    });    
+
+    return (
+        <li>
+            <Card className="expenses">
+                <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+                <ExpensesChart expenses={filteredExpenses} />
+                <ExpensesList items={filteredExpenses}/>
+            </Card>
+        </li>
+    );
+}
+
+export default Expenses;
